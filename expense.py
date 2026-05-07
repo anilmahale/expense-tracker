64b89618173d9888dec362bf338f715841cfad04
anil_mahale <anil_mahale@rediffmail.com>
2026-05-08 01:43:41 +0530

    Initial commit: expense tracker

---
+++ expense.py
@@ -0,0 +1,20 @@
+expenses = []
+
+def add_expense():
+    amount = float(input("Enter amount: "))
+    expenses.append(amount)
+    print("Expense added!")
+
+def show_total():
+    print("Total expense:", sum(expenses))
+
+while True:
+    print("\n1. Add Expense\n2. Show Total\n3. Exit")
+    choice = input("Choose: ")
+
+    if choice == "1":
+        add_expense()
+    elif choice == "2":
+        show_total()
+    elif choice == "3":
+        break
